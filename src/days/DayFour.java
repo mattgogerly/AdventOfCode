50720bef602a00a6940e21ce808fd96a59b951a9
mattgogerly <matthewgogerly@gmail.com>
2020-12-03 18:40:34 +0000

    Add stubbings for day 4

--- src/days/DayFour.java
+++ src/days/DayFour.java
@@ -1,2 +1,24 @@
-package days;public class DayFour {
+package days;
+
+import utils.InputUtils;
+
+public class DayFour implements Day {
+
+    public static void main(String[] args) {
+        new DayFour().printAnswers();
+    }
+
+    public DayFour() {
+
+    }
+
+    @Override
+    public Object partOne() {
+        return null;
+    }
+
+    @Override
+    public Object partTwo() {
+        return null;
+    }
 }
