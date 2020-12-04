ad9001a77f55808522ab4178bf160835036c0d35
mattgogerly <matthewgogerly@gmail.com>
2020-12-04 13:36:08 +0000

    Prepare for day 5

---
+++ src/days/DayFive.java
@@ -0,0 +1,22 @@
+package days;
+
+public class DayFive implements Day {
+
+    public static void main(String[] args) {
+        new DayFive().printAnswers();
+    }
+
+    public DayFive() {
+
+    }
+
+    @Override
+    public Object partOne() {
+        return null;
+    }
+
+    @Override
+    public Object partTwo() {
+        return null;
+    }
+}
