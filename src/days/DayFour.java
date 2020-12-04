aedcbaa7a341d4498a676f1a3f7db059c5a7bd85
mattgogerly <matthewgogerly@gmail.com>
2020-12-04 13:34:36 +0000

    Solutions for day 4

--- src/days/DayFour.java
+++ src/days/DayFour.java
@@ -1,6 +1,11 @@
 package days;
 
-import utils.InputUtils;
+import java.util.Arrays;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static utils.InputUtils.asString;
 
 public class DayFour implements Day {
 
@@ -8,17 +13,76 @@ public class DayFour implements Day {
         new DayFour().printAnswers();
     }
 
+    private static final Map<String, String> requiredFields = Map.of(
+            "byr", "^(19[2-9][0-9]|200[0-2])$",
+            "iyr", "^(201[0-9]|2020)$",
+            "eyr", "^(202[0-9]|2030)$",
+            "hgt", "^((1[5-8][0-9]|19[0-3])cm)|((59|6[0-9]|7[0-6])in)$",
+            "hcl", "^#[0-9a-f]{6}$",
+            "ecl", "^(amb|blu|brn|gry|grn|hzl|oth)$",
+            "pid", "^[0-9]{9}$"
+    );
+
     public DayFour() {
 
     }
 
     @Override
     public Object partOne() {
-        return null;
+        return passportStream()
+                .filter(Passport::validPartOne)
+                .count();
     }
 
     @Override
     public Object partTwo() {
-        return null;
+        return passportStream()
+                .filter(Passport::validPartTwo)
+                .count();
+    }
+
+    private Stream<Passport> passportStream() {
+        // each entry is separated by a double newline
+        String[] input = asString(4).split("\n\n");
+
+        // map each String (line) to a Passport
+        return Arrays.stream(input).map(Passport::new);
+    }
+
+
+    private static class Passport {
+
+        String[] values;
+
+        Passport(String p) {
+            // some passports are over multiple lines so join them with a space
+            // then split the passport into each of its fields (which are space separated)
+            values = p.replace('\n', ' ').split(" ");
+        }
+
+        public boolean validPartOne() {
+            // each field key is 3 letters so substring it and check we have all the required fields
+            return Arrays.stream(values)
+                    .map(v -> v.substring(0, 3))
+                    .collect(Collectors.toSet())
+                    .containsAll(requiredFields.keySet());
+        }
+
+        public boolean validPartTwo() {
+            boolean partTwoValid = Arrays.stream(values)
+                    .map(s -> s.split(":"))
+                    .allMatch(s -> validValue(s[0], s[1]));
+
+            return validPartOne() && partTwoValid;
+        }
+
+        private boolean validValue(String key, String value) {
+            if (!requiredFields.containsKey(key)) {
+                // not a required field so ignore it
+                return true;
+            }
+
+            return value.matches(requiredFields.get(key));
+        }
     }
 }
