ef99f91a588032838c4163252ddb8847c5d1dbbc
mattgogerly <matthewgogerly@gmail.com>
2020-12-08 18:45:20 +0000

    Solutions for day 9

---
+++ src/days/Day9.java
@@ -0,0 +1,18 @@
+package days;
+
+public class Day9 implements Day {
+
+    public static void main(String[] args) {
+        new Day9().printAnswers();
+    }
+
+    @Override
+    public Object partOne() {
+        return null;
+    }
+
+    @Override
+    public Object partTwo() {
+        return null;
+    }
+}
