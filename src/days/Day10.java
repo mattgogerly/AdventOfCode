b64ab115226fad476eeabf75f5a2a26d3c09b34f
mattgogerly <matthewgogerly@gmail.com>
2020-12-10 19:54:09 +0000

    Solutions for day 10

--- src/days/Day10.java
+++ src/days/Day10.java
@@ -1,5 +1,11 @@
 package days;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static utils.InputUtils.asIntegerStream;
+
 public class Day10 implements Day {
 
     public static void main(String[] args) {
@@ -8,11 +14,60 @@ public class Day10 implements Day {
 
     @Override
     public Object partOne() {
-        return null;
+        List<Integer> input = asIntegerStream(10)
+                .sorted()
+                .collect(Collectors.toList());
+
+        input.add(0, 0); // socket is 0 jolts
+
+        int oneCount = 0;
+        int threeCount = 1; // always a 3 jolt difference between last adapter and socket
+
+        for (int i = 0; i < input.size(); i++) {
+            if (i + 1 == input.size()) {
+                break;
+            }
+
+            int diff = input.get(i + 1) - input.get(i);
+            if (diff == 1) {
+                oneCount++;
+            } else if (diff == 3) {
+                threeCount++;
+            }
+        }
+
+        return oneCount * threeCount;
     }
 
     @Override
     public Object partTwo() {
-        return null;
+        List<Integer> input = asIntegerStream(10)
+                .sorted()
+                .collect(Collectors.toList());
+
+        input.add(0, 0); // socket is 0 jolts
+
+        int target = input.get(input.size() - 1) + 3; // device is max + 3 jolts
+        input.add(input.size(), target);
+
+        return getCombinations(input, target);
+    }
+
+    private long getCombinations(List<Integer> input, int target) {
+        long[] dp = new long[target + 1];
+
+        dp[0] = 1;
+
+        for (int i = 0; i < input.size(); i++) {
+            int adapter = input.get(i);
+
+            for (int j = i + 1; j < input.size() && j <= i + 3; j++) {
+                if (input.get(j) - adapter <= 3) {
+                    dp[input.get(j)] += dp[adapter];
+                }
+            }
+        }
+
+        return dp[target];
     }
 }
