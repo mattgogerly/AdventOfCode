cf682645917078b2e7d73a8f2887f696295c738c
mattgogerly <matthewgogerly@gmail.com>
2020-12-14 10:12:19 +0000

    Day 13 part 2 solution

--- src/days/Day13.java
+++ src/days/Day13.java
@@ -1,5 +1,8 @@
 package days;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static utils.InputUtils.asString;
 
 public class Day13 implements Day {
@@ -40,6 +43,75 @@ public class Day13 implements Day {
 
     @Override
     public Object partTwo() {
-        return null;
+        String[] input = asString(13).split("\n");
+        String timetable = input[1];
+
+        String[] buses = timetable.split(",");
+
+        List<Long> moduli = new ArrayList<>();
+        List<Long> residues = new ArrayList<>();
+
+        for (int i = 0; i < buses.length; i++) {
+            String bus = buses[i];
+            if (bus.equals("x")) {
+                continue;
+            }
+
+
+            long busId = Integer.parseInt(bus);
+            moduli.add(busId);
+            residues.add(busId - i);
+        }
+
+        return ChineseRemainderTheorem.chineseRemainder(moduli, residues);
+    }
+
+
+    private static class ChineseRemainderTheorem {
+
+        public static long chineseRemainder(List<Long> n, List<Long> a) {
+            long prod = n.stream().reduce(1L, (i, j) -> i * j);
+
+            long p, sm = 0;
+            for (int i = 0; i < n.size(); i++) {
+                long currentN = n.get(i);
+                long currentA = a.get(i);
+
+                if (currentN == 0L) {
+                    continue;
+                }
+
+                p = prod / currentN;
+                sm += currentA * mulInv(p, currentN) * p;
+            }
+
+            return sm % prod;
+        }
+
+        private static long mulInv(long a, long b) {
+            long b0 = b;
+            long x0 = 0;
+            long x1 = 1;
+
+            if (b == 1) {
+                return 1;
+            }
+
+            while (a > 1) {
+                long q = a / b;
+                long amb = a % b;
+                a = b;
+                b = amb;
+                long xqx = x1 - q * x0;
+                x1 = x0;
+                x0 = xqx;
+            }
+
+            if (x1 < 0) {
+                x1 += b0;
+            }
+
+            return x1;
+        }
     }
 }
