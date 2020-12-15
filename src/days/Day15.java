49e46331c0dd10db1814d842c98e9c5518ab888c
mattgogerly <matthewgogerly@gmail.com>
2020-12-15 12:41:13 +0000

    Change indexes to reduce confusion between turn number and array index and add more comments

--- src/days/Day15.java
+++ src/days/Day15.java
@@ -35,16 +35,19 @@ class Day15 extends Day {
 
         // add all but the last input
         // using i = 1, i-1 to reduce confusion on turn number vs array index
+        // for input 0,3,6 the mapping is:
+        // 0 -> 1 (turn 1)
+        // 3 -> 2 (turn 2)
         for (int i = 1; i < input.size(); i++) {
             store.put(input.get(i - 1), (long) i);
         }
 
-        // start with the last number in the input
+        // start with the last number in the input (6 in the example above)
         long lastNumber = input.get(input.size() - 1);
 
-        // turn starts at the last element of the input (the first number we need to consider)
+        // turn starts at the index of the last element of the input + 1 (3 in the example above)
         // note "turn < limit" since we set the lastNumber for the next iteration at the end of each iteration, so
-        // the number spoken on the "limit" turn is the value of lastNumber at the end of the iteration before it
+        // the number spoken on the "limit" turn is the value of lastNumber at the end of the limit - 1 turn
         for (long turn = input.size(); turn < limit; turn++) {
             long nextLastNumber = store.containsKey(lastNumber) ? turn - store.get(lastNumber) : 0;
             store.put(lastNumber, turn);
