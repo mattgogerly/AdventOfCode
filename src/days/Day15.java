e1f91fff14784ab28ecaeb8b290d1e4ca4d27ae8
mattgogerly <matthewgogerly@gmail.com>
2020-12-15 11:28:20 +0000

    Solutions for day 15

---
+++ src/days/Day15.java
@@ -0,0 +1,49 @@
+package days;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+import static utils.InputUtils.asString;
+
+public class Day15 implements Day {
+
+    public static void main(String[] args) {
+        new days.Day15().printAnswers();
+    }
+
+    @Override
+    public Object partOne() {
+        return determineLastNumber(2020);
+    }
+
+    @Override
+    public Object partTwo() {
+        return determineLastNumber(30000000);
+    }
+
+    private long determineLastNumber(long limit) {
+        List<Long> input = Arrays.stream(asString(15).split(","))
+                .map(Long::parseLong)
+                .collect(Collectors.toList());
+
+        Map<Long, Long> store = new HashMap<>();
+
+        // add all but the last input
+        for (int i = 0; i < input.size() - 1; i++) {
+            store.put(input.get(i), (long) i);
+        }
+
+        // start with the last number in the input
+        long lastNumber = input.get(input.size() - 1);
+
+        // confusing because arrays are 0 indexed, but first turn is the index of the last number in the input
+        // and the last turn is the limit - 1
+        for (long turn = input.size() - 1; turn < limit - 1; turn++) {
+            long nextLastNumber = store.containsKey(lastNumber) ? turn - store.get(lastNumber) : 0;
+            store.put(lastNumber, turn);
+            lastNumber = nextLastNumber;
+        }
+
+        return lastNumber;
+    }
+}
